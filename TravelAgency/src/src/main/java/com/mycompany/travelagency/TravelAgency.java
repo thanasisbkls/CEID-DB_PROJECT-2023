8c5ea6aa2333fc517405825cd73ee1000a9ac1aa
baknis <giorgosbaknis@gmail.com>
2023-09-12 03:38:56 +0300

    All procedures branch and offers ready. Also all the windows pera dothe work.

---
+++ TravelAgency/src/src/main/java/com/mycompany/travelagency/TravelAgency.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.travelagency;
+
+/**
+ *
+ * @author Baknis
+ */
+public class TravelAgency {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
